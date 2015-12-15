ed5bc822936efd09c2f491c049a9c74ac9716eea
riv <roller145@yandex.ru>
2015-12-15 21:35:38 +0300

    всем добрых снов

--- roller145/src/main/java/ru/fizteh/fivt/students/roller145/MiniORM/DataBaseService.java
+++ roller145/src/main/java/ru/fizteh/fivt/students/roller145/MiniORM/DataBaseService.java
@@ -1,14 +1,85 @@
 package ru.fizteh.fivt.students.roller145.MiniORM;
+import com.google.common.base.CaseFormat;
+import com.tngtech.java.junit.dataprovider.internal.DataConverter;
+import ru.fizteh.fivt.students.roller145.MiniORM.Annotations.Column;
+import ru.fizteh.fivt.students.roller145.MiniORM.Annotations.PrimaryKey;
+import ru.fizteh.fivt.students.roller145.MiniORM.Annotations.Table;
+
+import javax.management.OperationsException;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by riv on 14.12.15.
  */
 public class DataBaseService <T,K>{
+    static final String dataBaseNane = "RIVdatabase";
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private Field primaryKey;
+    private List<AnnotatedField> columns;
+    private Class<T> typeClass;
+    private String tableName;
 
-    DataBaseService(Class objectType){
+    private boolean hasTableYet;
 
+    private void connect() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        Class.forName("org.h2.Driver").newInstance();
+        connection = DriverManager.getConnection("jdbc:h2:test",
+                "sa", "");
+        statement = connection.createStatement();
+    }
 
-        throw new UnsupportedOperationException();
+    protected void finalize ( ) throws SQLException {
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+    DataBaseService(Class<T> elementClass) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        try{
+            this.connect();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        columns = new ArrayList<>();
+        typeClass = elementClass;
+        Table table = typeClass.getAnnotation(Table.class);
+        if (table == null) {
+            throw new IllegalArgumentException("Class should be annotated with Table");
+        }
+        tableName = table.name();
+        if (tableName.equals("")) {
+            tableName = "MY_TABLE";
+        }
+        for (Field elem : typeClass.getDeclaredFields()) {
+            if (elem.getAnnotation(Column.class) != null) {
+                columns.add(new AnnotatedField(elem));
+            }
+            if (elem.getAnnotation(PrimaryKey.class) != null) {
+                if (elem.getAnnotation(Column.class) == null) {
+                    throw new IllegalArgumentException("Primary key should be column");
+                }
+                if (primaryKey != null) {
+                    throw new IllegalArgumentException("Primary key should be one");
+                }
+                primaryKey = elem;
+            }
+        }
+        try{
+            resultSet = connection.getMetaData().getTables(null, null,
+                            CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_UNDERSCORE, tableName), null);
+            if (resultSet.next()) {
+                hasTableYet = true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     T queryById(K id) {
@@ -36,9 +107,20 @@ public class DataBaseService <T,K>{
         throw new UnsupportedOperationException();
     }
 
-    void createTable(){
-
-        throw new UnsupportedOperationException();
+    void createTable() throws OperationsException {
+        if (hasTableYet) {
+            throw new OperationsException("Невозможно повторно создать таблицу");
+        }
+        StringBuilder newRequest = new StringBuilder();
+        newRequest.append("CREATE TABLE ").append(tableName).append(" (");
+        for (AnnotatedField field : columns) {
+            newRequest.append(field.getColumnName()).append(" ");
+            newRequest.append(field.getH2Type()).append(" ");
+            if (field.getElement().isAnnotationPresent(PrimaryKey.class)) {
+                newRequest.append("NOT NULL PRIMARY KEY ");
+            }
+            newRequest.append(" , ");
+        }
     }
 
     void dropTable(){
